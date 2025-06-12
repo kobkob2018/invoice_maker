69569b4b4da5e6f554441c582a59f8a676f59087
kobkob2018 <yacov.avr@gmail.com>
2025-06-12 17:15:20 +0300

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,25 @@ import { useState } from 'react';
 function App() {
   const [status, setStatus] = useState('');
 
+
+  const invoiceData = {
+    type: "invoice",
+    description: "שירות ייעוץ",
+    subject: {
+      name: "יעקב עברי",
+      email: "yakov@example.com"
+    },
+    items: [
+      {
+        description: "שעת ייעוץ",
+        quantity: 1,
+        price: 300
+      }
+    ]
+  };
+
   const createInvoice = async () => {
-    const invoiceData = {
-      type: "invoice",
-      description: "שירות ייעוץ",
-      subject: {
-        name: "יעקב עברי",
-        email: "yakov@example.com"
-      },
-      items: [
-        {
-          description: "שעת ייעוץ",
-          quantity: 1,
-          price: 300
-        }
-      ]
-    };
+
 
     try {
       const res = await fetch('/api/invoice/create', {
@@ -31,20 +34,46 @@ function App() {
       setStatus(
         `📄 חשבונית #${data.number}
       📝 תיאור: ${data.description}
-      🕒 זמקקן: ${new Date(data.timestamp).toLocaleString()}`
+      🕒 זמן: ${new Date(data.timestamp).toLocaleString()}`
       );
       
     } catch (err) {
       setStatus(`שגיאה: ${err.message}`);
     }
   };
+  const createDemo = async () => {
 
+
+    try {
+      const res = await fetch('/api/demo/create', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(invoiceData)
+      });
+      const data = await res.json();
+      console.log('response from server:', data);
+      setStatus(
+        `📄 check1 #${data.number}
+      📝 check2: ${data.description}
+      🕒 check3: ${new Date(data.timestamp).toLocaleString()}`
+      );
+      
+    } catch (err) {
+      setStatus(`שגיאה: ${err.message}`);
+    }
+  };
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Invoice Maker</h1>
       <button onClick={createInvoice}>צור חשבונית</button>
       <p>{status}</p>
+
+      <h1>Demo Maker</h1>
+      <button onClick={createDemo}>צור חשבונית</button>
+      <p>{status}</p>
     </div>
+
+    
   );
 }
 
